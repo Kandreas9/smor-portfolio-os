998adbc8c7bb2f93cdd45678a5e0c6772fab9050
Kandreas9 <andkarido@gmail.com>
2023-03-05 06:55:06 +0100

    i hate safari 7

--- src/lib/header/headerMenu.svelte
+++ src/lib/header/headerMenu.svelte
@@ -58,7 +58,6 @@
 	}
 
 	ul {
-		bottom: 0;
 		display: flex;
 		justify-content: center;
 		align-items: center;
@@ -71,7 +70,7 @@
 		padding: 1rem 1rem;
 		border: 1px solid var(--light-color);
 		box-shadow: 0px 2.76626px 10.3735px 6.91564px rgba(0, 0, 0, 0.25);
-		position: fixed;
+		position: relative;
 		z-index: 10;
 		transition: width 500ms ease-in-out, border-radius 500ms ease-in-out,
 			padding 500ms ease-in-out;
